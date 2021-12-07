6624806137c2d4751bca177fb8ade78bae740458
Tim Ross <tim.ross@goteleport.com>
2021-12-07 11:11:01 -0500

    reduce connect to auth retry period for integration tests

--- integration/helpers.go
+++ integration/helpers.go
@@ -605,6 +605,7 @@ func (i *TeleInstance) GenerateConfig(t *testing.T, trustedSecrets []*InstanceSe
 	tconf.Kube.CheckImpersonationPermissions = nullImpersonationCheck
 
 	tconf.Keygen = testauthority.New()
+	tconf.RetryPeriod = defaults.HighResPollingPeriod
 	i.Config = tconf
 	return tconf, nil
 }
